64e2ed47c74ca87930478cdec0ef686161a3cfbf
subhambishwakarma <98475500+subhambishwakarma@users.noreply.github.com>
2023-04-12 22:46:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function threeSum(arr, target) {
 // write your code here
+	 let target = '';
+  while (arr > 0 || target.length < bits) {
+    target = (arr % 2) + target;
+    arr = Math.floor(arr / 2);
+  }
+  return target;
   
 }
 
